1e0d41dfe61ae2bd439cf762a4f840fb3533b17f
yakimax <sv1099912@gmail.com>
2022-09-26 01:38:43 +0530

    add

--- redux/redux_pocs/src/components/Balls.jsx
+++ redux/redux_pocs/src/components/Balls.jsx
@@ -1,21 +1,35 @@
-import React,{useState} from 'react' ;
+import React,{useReducer} from 'react' ;
+
+
+function reducer(state,action){
+    switch(action.type){
+        case "increment":
+            return state + 1 ;
+        case "decrement":
+            return state -1 ;
+        default :
+        console.log("wrong type") ;
+    }
+}
+
 
 function Balls() {
-    const [balls,setBalls] = useState(0) ;
-    let increment = ()=>{
-        setBalls(balls + 1) ;
-    }
-    let decrement = ()=>{
-        setBalls(balls - 1) ;
-    }
+    const [balls,dispatch] = useReducer(reducer,0) ;
+    // let increment = ()=>{
+    //     setBalls(balls + 1) ;
+    // }
+    // let decrement = ()=>{
+    //     setBalls(balls - 1) ;
+    // }
 
   return (
     <>
         <div>Balls</div>
         <div>No fo Balls: {balls} </div>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
+        <button onClick={dispatch ({type : "increment"})}>+</button>
+        <button onClick={dispatch ({type : "decrement"})}>-</button>
     </>
   )
 }
+
 export default Balls
