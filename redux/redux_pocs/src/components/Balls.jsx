418c55699f5d03356ef2c9bbe06cfa752991e4b3
yakimax <sv1099912@gmail.com>
2022-09-26 00:54:20 +0530

    add

--- redux/redux_pocs/src/components/Balls.jsx
+++ redux/redux_pocs/src/components/Balls.jsx
@@ -2,17 +2,17 @@ import React,{useState} from 'react' ;
 
 function Balls() {
     const [balls,setBalls] = useState(0) ;
-    increment = ()=>{
+    let increment = ()=>{
         setBalls(balls + 1) ;
     }
-    decrement = ()=>{
+    let decrement = ()=>{
         setBalls(balls - 1) ;
     }
 
   return (
     <>
         <div>Balls</div>
-        <div>No fo Balls: {balls}</div>
+        <div>No fo Balls: {balls} </div>
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
     </>
