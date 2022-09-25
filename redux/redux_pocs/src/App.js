faa4392df1cdd2307cb8bf984c24aff40ccf7baf
yakimax <sv1099912@gmail.com>
2022-09-26 00:23:03 +0530

    add

---
+++ redux/redux_pocs/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Balls from './components/Balls'
+
+function App() {
+  return (
+    <div>
+      <Balls/>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux/redux_pocs/src/components/Balls.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function balls() {
+  return (
+    <div>balls</div>
+  )
+}
+
+export default balls
