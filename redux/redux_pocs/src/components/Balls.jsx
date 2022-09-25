5d18020b5c82162f29eb0707885b0ced7206c1e8
yakimax <sv1099912@gmail.com>
2022-09-26 00:48:52 +0530

    add

--- redux/redux_pocs/src/components/Balls.jsx
+++ redux/redux_pocs/src/components/Balls.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react' ;
 
-function balls() {
+function Balls() {
+    const [balls,setBalls] = useState(0) ;
+    increment = ()=>{
+        setBalls(balls + 1) ;
+    }
+    decrement = ()=>{
+        setBalls(balls - 1) ;
+    }
+
   return (
-    <div>balls</div>
+    <>
+        <div>Balls</div>
+        <div>No fo Balls: {balls}</div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+    </>
   )
 }
-
-export default balls
+export default Balls
